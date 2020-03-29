864b918b782b89cc666578c8c8e88e2a24923955
Eric Grant <senatorericgrant@gmail.com>
2020-03-29 23:06:41 +0700

    refactor: setup root ViewController

--- MyTwitterDuplicate/Controller/MainTabController.swift
+++ MyTwitterDuplicate/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  MyTwitterDuplicate
 //
 //  Created by Eric Grant on 29.03.2020.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
 
-
 }
-
