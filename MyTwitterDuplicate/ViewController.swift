f97413b5a25fca802b648b9fc1d93dbbfbc75865
Eric Grant <senatorericgrant@gmail.com>
2020-03-29 22:20:14 +0700

    Initial Commit

---
+++ MyTwitterDuplicate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTwitterDuplicate
+//
+//  Created by Eric Grant on 29.03.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
