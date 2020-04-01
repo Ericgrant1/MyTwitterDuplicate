0de99d91f29b8517d3bc561f8bcaf990a87bce3a
Eric Grant <senatorericgrant@gmail.com>
2020-04-02 00:34:42 +0700

    feat: implement upload user profile image

---
+++ MyTwitterDuplicate/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MyTwitterDuplicate
+//
+//  Created by Eric Grant on 01.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
