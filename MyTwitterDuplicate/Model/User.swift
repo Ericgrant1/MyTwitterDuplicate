1940f2e96dbb706d5fa02cf580b5c78dac1078a4
Eric Grant <senatorericgrant@gmail.com>
2020-04-02 22:51:30 +0700

    feat: create custom User Object(Model)

---
+++ MyTwitterDuplicate/Model/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  MyTwitterDuplicate
+//
+//  Created by Eric Grant on 02.04.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let fullname: String
+    let email: String
+    let username: String
+    let profileImageUrl: String
+    let uid: String
+    
+    init(uid: String, dictionary: [String: AnyObject]) {
+        self.uid = uid
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    }
+}
