c2155b410215cfdd502cf5ef906640c489b534c8
Eric Grant <senatorericgrant@gmail.com>
2020-04-04 17:46:59 +0700

    feat: add Tweet timestamp

--- MyTwitterDuplicate/ViewModel/TweetViewModel.swift
+++ MyTwitterDuplicate/ViewModel/TweetViewModel.swift
@@ -17,12 +17,26 @@ struct TweetViewModel {
         return user.profileImageUrl
     }
     
+    var timestamp: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .month]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let now = Date()
+        return formatter.string(from: tweet.timestamp, to: now) ?? "10m"
+    }
+    
     var userInfoText: NSAttributedString {
         let title = NSMutableAttributedString(string: user.fullname,
                                               attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
         title.append(NSAttributedString(string: " @\(user.username)",
             attributes: [.font: UIFont.systemFont(ofSize: 14),
                          .foregroundColor: UIColor.lightGray]))
+        
+        title.append(NSAttributedString(string: "・\(timestamp)",
+        attributes: [.font: UIFont.systemFont(ofSize: 14),
+                     .foregroundColor: UIColor.lightGray]))
+        
         return title
     }
     
