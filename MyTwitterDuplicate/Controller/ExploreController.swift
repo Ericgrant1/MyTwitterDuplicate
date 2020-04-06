e8c6a91a42ad8a3da0f6b723481b9d834e807175
Eric Grant <senatorericgrant@gmail.com>
2020-04-06 15:04:01 +0700

    feat: add Search page UI

--- MyTwitterDuplicate/Controller/ExploreController.swift
+++ MyTwitterDuplicate/Controller/ExploreController.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 
-class ExploreController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class ExploreController: UITableViewController {
     
     // MARK: - Properties
     
@@ -26,5 +28,20 @@ class ExploreController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Explore"
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+extension ExploreController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        return cell
     }
 }
