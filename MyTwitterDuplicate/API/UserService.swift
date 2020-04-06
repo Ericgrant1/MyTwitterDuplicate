1f3e88628e4bd6ac6a36423d31cdcd757239653f
Eric Grant <senatorericgrant@gmail.com>
2020-04-06 16:04:34 +0700

    feat: add fetching & displaying Users

--- MyTwitterDuplicate/API/UserService.swift
+++ MyTwitterDuplicate/API/UserService.swift
@@ -12,13 +12,25 @@ import Firebase
 struct UserService {
     static let shared = UserService()
     
-    func fetchUser(uid: String, conpletion: @escaping(User) -> Void) {
+    func fetchUser(uid: String, completion: @escaping(User) -> Void) {
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             
             let user = User(uid: uid, dictionary: dictionary)
-            conpletion(user)
+            completion(user)
+        }
+    }
+    
+    func fetchUsers(completion: @escaping([User]) -> Void) {
+        var users = [User]()
+        REF_USERS.observe(.childAdded) { snapshot in
+            let uid = snapshot.key
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            
+            let user = User(uid: uid, dictionary: dictionary)
+            users.append(user)
+            completion(users)
         }
     }
 }
