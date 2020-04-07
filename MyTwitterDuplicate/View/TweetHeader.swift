87e0125d98bdff6e35d87c12e4caaf13002e02b9
Eric Grant <senatorericgrant@gmail.com>
2020-04-07 14:08:34 +0700

    feat: implement Tweet Header UI

--- MyTwitterDuplicate/View/TweetHeader.swift
+++ MyTwitterDuplicate/View/TweetHeader.swift
@@ -12,16 +12,167 @@ class TweetHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Pavel Grant"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "senator"
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Some test caption from senator for now"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.textAlignment = .left
+        label.text = "7:22 AM - 4/07/2020"
+        return label
+    }()
+    
+    private lazy var optionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "down_arrow_24pt"), for: .normal)
+        button.tintColor = .lightGray
+        button.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "2 Retweets"
+        
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "0 Likes"
+        
+        return label
+    }()
+    
+    private lazy var statsView: UIView = {
+        let view = UIView()
+        
+        let dividerOne = UIView()
+        dividerOne.backgroundColor = .systemGroupedBackground
+        view.addSubview(dividerOne)
+        dividerOne.anchor(top: view.topAnchor,
+                          left: view.leftAnchor,
+                          right: view.rightAnchor,
+                          paddingLeft: 8,
+                          height: 1.0)
+        
+        let stack = UIStackView(arrangedSubviews: [retweetLabel, likesLabel])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.centerY(inView: view)
+        stack.anchor(left: view.leftAnchor,
+                     paddingLeft: 16)
+        
+        let dividerTwo = UIView()
+        dividerTwo.backgroundColor = .systemGroupedBackground
+        view.addSubview(dividerTwo)
+        dividerTwo.anchor(left: view.leftAnchor,
+                          bottom: view.bottomAnchor,
+                          right: view.rightAnchor,
+                          paddingLeft: 8,
+                          height: 1.0)
+        
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemIndigo
+        let labelStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel])
+        labelStack.axis = .vertical
+        labelStack.spacing = -6
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, labelStack])
+        stack.spacing = 12
+        
+        addSubview(stack)
+        stack.anchor(top: topAnchor,
+                     left: leftAnchor,
+                     paddingTop: 16,
+                     paddingLeft: 16)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: stack.bottomAnchor,
+                            left: leftAnchor,
+                            right: rightAnchor,
+                            paddingTop: 20,
+                            paddingLeft: 16,
+                            paddingRight: 16)
+        
+        addSubview(dateLabel)
+        dateLabel.anchor(top: captionLabel.bottomAnchor,
+                         left: leftAnchor,
+                         paddingTop: 20,
+                         paddingLeft: 16)
+        
+        addSubview(optionButton)
+        optionButton.centerY(inView: stack)
+        optionButton.anchor(right: rightAnchor,
+                            paddingRight: 8)
+        
+        addSubview(statsView)
+        statsView.anchor(top: dateLabel.bottomAnchor,
+                         left: leftAnchor,
+                         right: rightAnchor,
+                         paddingTop: 20,
+                         height: 40)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleProfileImageTapped() {
+        print("DEBUG: Go to user profile..")
+    }
+    
+    @objc func showActionSheet() {
+        print("DEBUG: Handle show action sheet..")
     }
 }
 
