b7b3915c77bdf00788e4e2fcbf69cebbf1e29320
Eric Grant <senatorericgrant@gmail.com>
2020-04-07 19:49:45 +0700

    Revert "fix: dynamic Cell sizing"
    This reverts commit 3c78192090583e49e5b8b8dc4c5bd35bc06fb813.

--- MyTwitterDuplicate/Controller/FeedController.swift
+++ MyTwitterDuplicate/Controller/FeedController.swift
@@ -102,9 +102,7 @@ extension FeedController {
 
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let viewModel = TweetViewModel(tweet: tweets[indexPath.row])
-        let height = viewModel.size(forWidth: view.frame.width).height
-        return CGSize(width: view.frame.width, height: height + 80)
+        return CGSize(width: view.frame.width, height: 120)
     }
 }
 
--- MyTwitterDuplicate/View/TweetHeader.swift
+++ MyTwitterDuplicate/View/TweetHeader.swift
@@ -153,7 +153,7 @@ class TweetHeader: UICollectionReusableView {
         captionLabel.anchor(top: stack.bottomAnchor,
                             left: leftAnchor,
                             right: rightAnchor,
-                            paddingTop: 12,
+                            paddingTop: 20,
                             paddingLeft: 16,
                             paddingRight: 16)
         
@@ -172,7 +172,7 @@ class TweetHeader: UICollectionReusableView {
         statsView.anchor(top: dateLabel.bottomAnchor,
                          left: leftAnchor,
                          right: rightAnchor,
-                         paddingTop: 12,
+                         paddingTop: 20,
                          height: 40)
         
         let actionStack = UIStackView(arrangedSubviews: [commentButton,
