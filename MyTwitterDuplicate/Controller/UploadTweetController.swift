ed0c824e5469f653f589d2bbd04004063abcea5b
Eric Grant <senatorericgrant@gmail.com>
2020-04-08 18:20:44 +0700

    feat: add uploading Reply to Firebase

--- MyTwitterDuplicate/Controller/UploadTweetController.swift
+++ MyTwitterDuplicate/Controller/UploadTweetController.swift
@@ -69,13 +69,6 @@ class UploadTweetController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
-        
-        switch config {
-        case .tweet:
-            print("DEBUG: Confid is tweet")
-        case .reply(let tweet):
-            print("DEBUG: Replaying to \(tweet.caption)")
-        }
     }
     
     // MARK: - Selectors
@@ -86,7 +79,7 @@ class UploadTweetController: UIViewController {
     
     @objc func handleUploadTweet() {
         guard let caption = captionTextView.text else { return }
-        TweetService.shared.uploadTweet(caption: caption) { (error, ref) in
+        TweetService.shared.uploadTweet(caption: caption, type: config) { (error, ref) in
             if let error = error {
                 print("DEBUG: Failed to upload tweet with error \(error.localizedDescription)")
                 return
