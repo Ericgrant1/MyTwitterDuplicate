914c0deda46cf064f9e3d6c8f78c690776949902
Eric Grant <senatorericgrant@gmail.com>
2020-04-13 01:46:38 +0700

    feat: add fetching User replies

--- MyTwitterDuplicate/API/TweetService.swift
+++ MyTwitterDuplicate/API/TweetService.swift
@@ -29,7 +29,10 @@ struct TweetService {
             }
         case .reply(let tweet):
             REF_TWEET_REPLIES.child(tweet.tweeetID).childByAutoId()
-                .updateChildValues(values, withCompletionBlock: completion)
+                .updateChildValues(values) { (error, ref) in
+                    guard let replyKey = ref.key else { return }
+                    REF_USER_REPLIES.child(uid).updateChildValues([tweet.tweeetID: replyKey], withCompletionBlock: completion)
+            }
         }
     }
     
@@ -65,9 +68,30 @@ struct TweetService {
         REF_TWEETS.child(tweetID).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: Any] else { return }
             guard let uid = dictionary["uid"] as? String else { return }
+            
             UserService.shared.fetchUser(uid: uid) { user in
                 let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
                 completion(tweet)
+            }
+        }
+    }
+    
+    func fetchReplies(forUser user: User, completion: @escaping([Tweet]) -> Void) {
+        var replies = [Tweet]()
+        
+        REF_USER_REPLIES.child(user.uid).observe(.childAdded) { snapshot in
+            let tweetKey = snapshot.key
+            guard let replyKey = snapshot.value as? String else { return }
+            
+            REF_TWEET_REPLIES.child(tweetKey).child(replyKey).observeSingleEvent(of: .value) { snapshot in
+                guard let dictionary = snapshot.value as? [String: Any] else { return }
+                guard let uid = dictionary["uid"] as? String else { return }
+                
+                UserService.shared.fetchUser(uid: uid) { user in
+                    let tweet = Tweet(user: user, tweetID: tweetKey, dictionary: dictionary)
+                    replies.append(tweet)
+                    completion(replies)
+                }
             }
         }
     }
