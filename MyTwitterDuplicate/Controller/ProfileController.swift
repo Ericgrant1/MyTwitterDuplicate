531a21feb23caa00dcb1eb13151dbe1a173be932
Eric Grant <senatorericgrant@gmail.com>
2020-04-13 16:42:06 +0700

    feat: add setting up Reply Label

--- MyTwitterDuplicate/Controller/ProfileController.swift
+++ MyTwitterDuplicate/Controller/ProfileController.swift
@@ -78,10 +78,6 @@ class ProfileController: UICollectionViewController {
     func fetchReplies() {
         TweetService.shared.fetchReplies(forUser: user) { tweets in
             self.replies = tweets
-            
-            self.replies.forEach { reply in
-                print("DEBUG: replying to \(reply.replyingTo)")
-            }
         }
     }
     
@@ -149,8 +145,13 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let viewModel = TweetViewModel(tweet: currentDataSource[indexPath.row])
-        let height = viewModel.size(forWidth: view.frame.width).height
-        return CGSize(width: view.frame.width, height: height + 80)
+        var height = viewModel.size(forWidth: view.frame.width).height + 80
+        
+        if currentDataSource[indexPath.row].isReply {
+            height += 20
+        }
+        
+        return CGSize(width: view.frame.width, height: height)
     }
 }
 
