6d5cf874a1d037e0e4b285eb6e4c3e350d9efcbf
Eric Grant <senatorericgrant@gmail.com>
2020-04-14 17:26:46 +0700

    feat: setup Edit Profile Header

--- MyTwitterDuplicate/Controller/EditProfileController.swift
+++ MyTwitterDuplicate/Controller/EditProfileController.swift
@@ -13,6 +13,7 @@ class EditProfileController: UITableViewController {
     // MARK: - Properties
     
     private let user: User
+    private lazy var headerView = EditProfileHeader(user: user)
     
     // MARK: - Lifecycle
     
@@ -29,6 +30,7 @@ class EditProfileController: UITableViewController {
         super.viewDidLoad()
         
         configureNavigationBar()
+        configureTableView()
     }
     
     // MARK: - Selectors
@@ -57,4 +59,19 @@ class EditProfileController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDone))
         navigationItem.rightBarButtonItem?.isEnabled = false
     }
+    
+    func configureTableView() {
+        tableView.tableHeaderView = headerView
+        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 180)
+        tableView.tableFooterView = UIView()
+        
+        headerView.delegate = self
+    }
+}
+
+extension EditProfileController: EditProfileHeaderDelegate {
+    func didTapChangeProfilePhoto() {
+        print("DEBUG: Handle change photo..")
+    }
+    
 }
