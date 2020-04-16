83263ea95b5ecb653f75e8a4bd696127e3fcd5f3
Eric Grant <senatorericgrant@gmail.com>
2020-04-16 20:19:11 +0700

    feat: go to User Profile on Mention tap

--- MyTwitterDuplicate/View/TweetCell.swift
+++ MyTwitterDuplicate/View/TweetCell.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import ActiveLabel
 
 protocol TweetCellDelegate: class {
     func handleProfileImageTapped(_ cell: TweetCell)
     func handleReplyTapped(_ cell: TweetCell)
     func handleLikeTapped(_ cell: TweetCell)
+    func handleFetchUser(withUsername username: String)
 }
 
 class TweetCell: UICollectionViewCell {
@@ -41,17 +43,20 @@ class TweetCell: UICollectionViewCell {
         return iv
     }()
     
-    private let replyLabel: UILabel = {
-        let label = UILabel()
+    private let replyLabel: ActiveLabel = {
+        let label = ActiveLabel()
         label.textColor = .lightGray
         label.font = UIFont.systemFont(ofSize: 12)
+        label.mentionColor = .twitterBlue
         return label
     }()
     
-    private let captionLabel: UILabel = {
-        let label = UILabel()
+    private let captionLabel: ActiveLabel = {
+        let label = ActiveLabel()
         label.font = UIFont.systemFont(ofSize: 14)
         label.numberOfLines = 0
+        label.mentionColor = .twitterBlue
+        label.hashtagColor = .twitterBlue
         return label
     }()
     
@@ -144,6 +149,8 @@ class TweetCell: UICollectionViewCell {
                              bottom: bottomAnchor,
                              right: rightAnchor,
                              height: 1)
+        
+        configureMentionHandler()
     }
     
     required init?(coder: NSCoder) {
@@ -190,4 +197,9 @@ class TweetCell: UICollectionViewCell {
         replyLabel.text = viewModel.replyText
     }
     
+    func configureMentionHandler() {
+        captionLabel.handleMentionTap { username in
+            self.delegate?.handleFetchUser(withUsername: username)
+        }
+    }
 }
