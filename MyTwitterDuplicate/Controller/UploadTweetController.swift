28e4e096e6165a1b45a1533be7e7d92a36308f5a
Eric Grant <senatorericgrant@gmail.com>
2020-04-16 18:20:33 +0700

    feat: install & implementation Active Label

--- MyTwitterDuplicate/Controller/UploadTweetController.swift
+++ MyTwitterDuplicate/Controller/UploadTweetController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ActiveLabel
 
 class UploadTweetController: UIViewController {
     
@@ -43,10 +44,11 @@ class UploadTweetController: UIViewController {
         return iv
     }()
     
-    private lazy var replyLabel: UILabel = {
-        let label = UILabel()
+    private lazy var replyLabel: ActiveLabel = {
+        let label = ActiveLabel()
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = .lightGray
+        label.mentionColor = .twitterBlue
         label.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
         return label
     }()
@@ -69,6 +71,7 @@ class UploadTweetController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        configureMentionHandler()
     }
     
     // MARK: - Selectors
@@ -134,6 +137,12 @@ class UploadTweetController: UIViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
+    }
+    
+    func configureMentionHandler() {
+        replyLabel.handleMentionTap { mention in
+            print("DEBUG: Mentioned user is \(mention)")
+        }
     }
 }
 
