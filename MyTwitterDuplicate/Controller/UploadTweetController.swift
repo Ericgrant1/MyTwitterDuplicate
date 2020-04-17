7cda8d93e050cd5841d3e93ecb6f7ccdc15917e1
Eric Grant <senatorericgrant@gmail.com>
2020-04-17 16:22:46 +0700

    refactor: upload Notification

--- MyTwitterDuplicate/Controller/UploadTweetController.swift
+++ MyTwitterDuplicate/Controller/UploadTweetController.swift
@@ -89,7 +89,9 @@ class UploadTweetController: UIViewController {
             }
             
             if case .reply(let tweet) = self.config {
-                NotificationService.shared.uploadNotification(type: .reply, tweet: tweet)
+                NotificationService.shared.uploadNotification(toUser: tweet.user,
+                                                              type: .reply,
+                                                              tweetID: tweet.tweeetID)
             }
             
             self.dismiss(animated: true, completion: nil)
@@ -97,6 +99,22 @@ class UploadTweetController: UIViewController {
     }
     
     // MARK: - API
+    
+    fileprivate func uploadMentionNotification(forCaption caption: String, tweetID: String?) {
+        guard caption.contains("@") else { return }
+        let words = caption.components(separatedBy: .whitespacesAndNewlines)
+        
+        words.forEach { word in
+            guard word.hasPrefix("@") else { return }
+            
+            var username = word.trimmingCharacters(in: .symbols)
+            username = username.trimmingCharacters(in: .punctuationCharacters)
+            
+            UserService.shared.fetchUser(withUsername: username) { mentionedUser in
+                NotificationService.shared.uploadNotification(toUser: mentionedUser, type: .mention, tweetID: tweetID)
+            }
+        }
+    }
     
     // MARK: - Helpers
     
